a4b8d4df7f515b540ccf19a53d2af2281490a6db
akhmedovkb <akhmedovkb@gmail.com>
2025-07-09 17:25:48 +0500

    Delete travel-app-starter directory

--- travel-app-starter/backend/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerProvider } = require('../controllers/authController');
-
-router.post('/register', registerProvider);
-
-module.exports = router;
