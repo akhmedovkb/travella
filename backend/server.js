7fc369dfc0b0e45bd8fc0dc15e756fb76c8e739f
akhmedovkb <akhmedovkb@gmail.com>
2025-07-10 21:28:22 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
-import pool from './db.js';
+import pool, { initDB } from './db.js'; // <–– Добавили initDB сюда
 
 dotenv.config();
 const app = express();
