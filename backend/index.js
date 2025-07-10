a65a3b508c4c2d05b2ef5817f1a4391df55f9703
akhmedovkb <akhmedovkb@gmail.com>
2025-07-10 21:11:56 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Импортируем роуты
+const providerRoutes = require('./routes/providerRoutes');
+
+// Middleware
+app.use(cors({
+  origin: ['https://frontend-komil.vercel.app', 'https://frontend-six-ivory-24.vercel.app'], // 👈 для CORS
+  credentials: true
+}));
+app.use(express.json());
+
+// Роуты
+app.use('/api/providers', providerRoutes);
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`✅ Сервер запущен на порту ${PORT}`);
+});
