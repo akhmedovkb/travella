84b27db38f16fb559a6a2b6983d7e72230f78f5e
akhmedovkb <akhmedovkb@gmail.com>
2025-07-10 22:55:46 +0500

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,10 @@
 // routes/authRoutes.js
 import express from 'express';
-import { registerProvider, loginProvider } from '../controllers/authController.js';
+import { registerProvider } from '../controllers/authController.js';
 
 const router = express.Router();
 
-// Регистрация поставщика
+// POST /api/providers/register
 router.post('/providers/register', registerProvider);
-
-// Логин поставщика
-router.post('/providers/login', loginProvider);
 
 export default router;
