2fc345c83e4ad3b2949090a18bdda18ffd73c8de
akhmedovkb <akhmedovkb@gmail.com>
2025-07-11 19:26:04 +0500

    Update clientAuth.js

--- backend/middleware/clientAuth.js
+++ backend/middleware/clientAuth.js
@@ -1,17 +1,16 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET;
 
-function clientAuth(req, res, next) {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ error: 'Нет токена' });
+const clientAuth = (req, res, next) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Нет токена, доступ запрещен' });
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.clientId = decoded.clientId;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.client = decoded;
     next();
   } catch (err) {
     res.status(401).json({ error: 'Неверный токен' });
   }
-}
+};
 
 module.exports = clientAuth;
