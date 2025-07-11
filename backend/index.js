7ee0e0857a8a83deb7f41bc445893741b94a8ff5
akhmedovkb <akhmedovkb@gmail.com>
2025-07-11 19:16:17 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,15 @@ const providerRoutes = require('./routes/providerRoutes');
 const clientRoutes = require('./routes/clientRoutes');
 
 dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 5000;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/providers', providerRoutes);
 app.use('/api/clients', clientRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Travella API is running');
-});
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
