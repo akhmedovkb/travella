6b7260206faebc09e40e1e87ab38b0382987ad87
akhmedovkb <akhmedovkb@gmail.com>
2025-07-17 20:01:44 +0500

    Update providerRoutes.js

--- backend/routes/providerRoutes.js
+++ backend/routes/providerRoutes.js
@@ -1,26 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {
-  registerProvider,
-  loginProvider,
-  getProviderProfile,
-  updateProviderProfile,
-  createService,
-  getServices,
-  updateService,
-  deleteService,
-} = require('../controllers/providerController');
-const verifyToken = require('../middleware/providerAuth');
+const { loginProvider } = require('../controllers/providerController');
 
-router.post('/register', registerProvider);
 router.post('/login', loginProvider);
-
-router.get('/profile', verifyToken, getProviderProfile);
-router.put('/profile', verifyToken, updateProviderProfile);
-
-router.post('/services', verifyToken, createService);
-router.get('/services', verifyToken, getServices);
-router.put('/services/:id', verifyToken, updateService);
-router.delete('/services/:id', verifyToken, deleteService);
 
 module.exports = router;
